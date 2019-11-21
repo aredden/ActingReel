3f24152b5286f04a368e5249d11666da9e1c5af5
Alex Redden <alexredden00@gmail.com>
2019-11-21 15:18:50 -0500

    topnav on all pages / active topnav css added on page load

---
+++ actingreel/ui.apps/src/main/content/jcr_root/apps/actingreel/clientlibs/clientlib-site/components/topnav/topnav.js
@@ -0,0 +1,1 @@
+document.getElementById("Nav "+document.title).classList.add("active");
--- actingreel/ui.apps/src/main/content/jcr_root/apps/actingreel/components/structure/topnav/TopNav.java
+++ actingreel/ui.apps/src/main/content/jcr_root/apps/actingreel/components/structure/topnav/TopNav.java
@@ -1,11 +1,10 @@
 package apps.actingreel.components.structure.topnav;
 
 import java.util.*;
-import java.util.Iterator;
+
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageFilter;
 import com.day.cq.wcm.api.PageManager;
-import org.apache.sling.api.resource.ResourceResolver;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 
@@ -14,27 +13,35 @@ public class TopNav extends WCMUsePojo{
     private Page rootPage;
     private PageManager pageMgr;
     private String rootString;
-    private ResourceResolver resolver;
+    private Page currentPage;
     // Initializes the navigation
     @Override
     public void activate() throws Exception {
     	
         rootString = getProperties().get("parentPage",null);
+        currentPage = getCurrentPage();
+        pageMgr = getPageManager();
         
-        if (rootString == null) {
-        	rootPage = getCurrentPage();
-        } else {
-        	resolver = getRequest().getResourceResolver();
-        	pageMgr = resolver.adaptTo(PageManager.class);
-        	rootPage = pageMgr.getPage(rootString).adaptTo(Page.class);
-        }
-        
+        findRootPage(rootString);
+        populatePageList();
+
+    }
+    
+    private void populatePageList() {
         Iterator<Page> childPages = rootPage.listChildren(new PageFilter(getRequest()));
         while (childPages.hasNext()) {
 			items.add(childPages.next());
 	   	}
     }
-
+    
+    private void findRootPage(String rootStr) {
+        if (rootString == null) {
+        	rootPage = currentPage;
+        } else {
+        	rootPage = pageMgr.getPage(rootString).adaptTo(Page.class);
+        }
+    }
+    
     // Returns the navigation items
     public List<Page> getItems() {
         return items;
@@ -45,4 +52,5 @@ public class TopNav extends WCMUsePojo{
     		return(getCurrentPage());
     	} else return rootPage;
     }
+    
 }
